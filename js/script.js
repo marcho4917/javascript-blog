f5a6036b066e54ac5e4188e88153833f4dea4a47
Martyna Chojnowska <martynachojnowska12@gmail.com>
2020-01-15 19:04:42 +0100

    add functions search and view article

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@
   });*/
 
   const titleClickHandler = function(event) {
+    event.preventDefault();
     const clickedElement = this;
     console.log('Link was clicked!');
 
@@ -14,11 +15,10 @@
     for(let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
+
     /*add class 'active' to the clicked link*/
 
     clickedElement.classList.add('active');
-    console.log('clickedElement:', clickedElement);
-
 
     /*remove class 'active' from all articles*/
 
@@ -27,11 +27,20 @@
     for (let activeArticle of activeArticles) {
         activeArticle.classList.remove('active');
     }
+
     /*get 'href' attribute from the clicked link*/
+
+    const articleSelector = clickedElement.getAttribute('href');
+    console.log(articleSelector);
 
     /*find the correct article using the selector (value of 'href' attribute)*/
 
+    const targetArticle = document.querySelector(articleSelector);
+    console.log(targetArticle);
+
     /*add class 'active' to the correct article*/
+
+    targetArticle.classList.add('active');
   }
   
   const links = document.querySelectorAll('.titles a');
